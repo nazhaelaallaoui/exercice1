1ef63e8ffe7b8962e6526c0da659e51dcb5a0169
nazhaelaallaoui <elaallaouinazha@gmail.com>
2024-11-02 17:20:54 +0100

    Add files via upload

---
+++ exercice 1.py
@@ -0,0 +1,7 @@
+
+tableau1 = [4, 8, 7, 9, 1, 5, 4, 6]
+tableau2 = [7, 6, 5, 2, 3, 8, 1, 4]
+tableau3 = []
+for i in range(len(tableau1)):
+    tableau3.append(tableau1[i] + tableau2[i])
+print("Tableau 3:", tableau3)
